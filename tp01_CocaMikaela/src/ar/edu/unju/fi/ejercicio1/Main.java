653f7b39d61f1c6788093ca81ed4e459dc817bec
seba_ <seba_@Compu>
2024-04-04 17:38:25 -0300

    Finalizacion ejercicio 1

--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,7 +3,15 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenidos a programacion visual");
+		
+		float primerNumero=8;
+		float segundoNumero=5;
+		float tercerNumero=1;
+		float cuartoNumero=20;
+		float quintoNumero=9;
+		float promedio = (primerNumero + segundoNumero + tercerNumero+ cuartoNumero + quintoNumero)/5;
+		System.out.println("variables:" + primerNumero + ", " + segundoNumero + ", " + tercerNumero + ", " + cuartoNumero + ", " + quintoNumero);
+		System.out.println("El promedio es:" + promedio);
 
 	}
 
