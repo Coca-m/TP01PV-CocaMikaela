74dbedd9663542622f7644675c248d7d1ff1cbbc
seba_ <seba_@Compu>
2024-04-09 02:20:30 -0300

    Finalizacion del ejercicio 2

--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -4,7 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		String pais = "Argentina";
+		int edad_persona = 28;
+		double Altura_edificio = 25.44;
+		double precio_producto = 699.99;
+		long telefono = 3884095788l;
+		double coseno = 0.999;
+		
 
+		System.out.println("Nombre de un pais: "+ pais);
+		System.out.println("edad de una persona: "+ edad_persona+ " anios");
+		System.out.println("altura de un edificio: "+ Altura_edificio+ "m");
+		System.out.println("precio de un producto: "+ precio_producto+ "$");
+		System.out.println("numero de telefono: " + telefono);
+		System.out.println("calculo del coseno de 0,5: "+ coseno);
 	}
 
 }
