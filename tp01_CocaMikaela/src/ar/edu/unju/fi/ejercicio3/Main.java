274a13ba2af4bb39d5b56abbba51e1e6137f6373
seba_ <seba_@Compu>
2024-04-09 02:50:47 -0300

    fin del ejercicio 3

--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio3/Main.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -1,10 +1,24 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ingrese un numero entero");
+		int num = scanner.nextInt();
+		if (num%2 == 0) {
+			num = num*3;
+			System.out.println("el numero es par: "+ num);
+		}
+		else {
+			num = num*2;
+		    System.out.println("el numero es impar: "+ num);
+		}
+		
+	
 	}
 
 }
