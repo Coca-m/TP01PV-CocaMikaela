4689c5bf8daf7f739e626f2d4d97fd6d6afd8ccc
seba_ <seba_@Compu>
2024-04-10 15:42:43 -0300

    fin del ejercicio 4

--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio4/Main.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -1,10 +1,35 @@
 package ar.edu.unju.fi.ejercicio4;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		int factorial=1;
+		
+		System.out.println("Ingrese un numero entero del 0 al 10" );
+		int num = scan.nextInt();
+		 if (num>=0 && num<=10) {
+			if (num==0) {
+			factorial = 1;
+			}
+			else
+			{factorial=num;
+				while(num>=2){
+					num--;
+					factorial = factorial*num;
+				}
+                   				
+			}
+		System.out.println("el factorial del numero ingresado es: "+ factorial);
+		 }
+		 else
+			 System.out.println("el numero no cumple con el rango especificado");
+		
+		
+	
 	}
 
 }
