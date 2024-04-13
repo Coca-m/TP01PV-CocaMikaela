2dd7d136d034a6beb77a2926c16ed5fb248e36bc
seba_ <seba_@Compu>
2024-04-13 18:05:05 -0300

    finalizacion ejercicio 6

--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio6/Main/Main.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio6/Main/Main.java
@@ -1,10 +1,83 @@
 package ar.edu.unju.fi.ejercicio6.Main;
 
+import java.util.*;
+import ar.edu.unju.fi.ejercicio6.Model.Persona;
+import java.time.LocalDate;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Persona persona = new Persona();
+		int edad;
+		
+		System.out.println("ingrese dni");
+		persona.setDni(scan.nextInt());
+		System.out.println("ingrese nombre");
+		persona.setNombre(scan.next());
+		System.out.println("ingrese fecha de nacimiento");
+		System.out.println("ingrese anio");
+		int anio = scan.nextInt();
+		System.out.println("ingrese mes");
+		int mes = scan.nextInt();
+		System.out.println("ingrese dia");
+		int dia = scan.nextInt();
+		persona.setFecha(anio, mes, dia);
+		edad = persona.calcular_edad(persona.getFecha());
+		System.out.println("ingrese provincia");
+		persona.setProvincia(scan.next());
+		persona.getdatos(edad);
+		
+		
+		System.out.println("\ningrese los datos de la segunda persona");
+		System.out.println("ingrese dni");
+		int DNI2 = (scan.nextInt());
+		System.out.println("ingrese nombre");
+		String nombre2 = (scan.next());
+		System.out.println("ingrese fecha de nacimiento");
+		System.out.println("ingrese anio");
+		int anio2 = scan.nextInt();
+		System.out.println("ingrese mes");
+		int mes2 = scan.nextInt();
+		System.out.println("ingrese dia");
+		int dia2 = scan.nextInt();
+		LocalDate fecha_nac2 = LocalDate.of(anio2, mes2, dia2);
+		System.out.println("ingrese provincia");
+		String provincia2 = (scan.next());
+		
+		Persona persona2 = new Persona(DNI2,nombre2,fecha_nac2,provincia2);
+		
+		int edad2 = persona.calcular_edad(persona2.getFecha());
+		persona2.getdatos(edad2);
+		
+		
+		System.out.println("\ningrese los datos de la tercera persona");
+		System.out.println("ingrese dni");
+		int DNI3 = (scan.nextInt());
+		System.out.println("ingrese nombre");
+		String nombre3 = (scan.next());
+		System.out.println("ingrese fecha de nacimiento");
+		System.out.println("ingrese anio");
+		int anio3 = scan.nextInt();
+		System.out.println("ingrese mes");
+		int mes3 = scan.nextInt();
+		System.out.println("ingrese dia");
+		int dia3 = scan.nextInt();
+		LocalDate fecha_nac3 = LocalDate.of(anio3, mes3, dia3);
+		
+		Persona persona3 = new Persona(DNI3, nombre3, fecha_nac3);
+		int edad3 = persona.calcular_edad(persona3.getFecha());
+		persona3.getdatos(edad3);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio6/Model/Persona.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio6/Model/Persona.java
@@ -1,5 +1,112 @@
 package ar.edu.unju.fi.ejercicio6.Model;
 
-public class Persona {
+import java.time.LocalDate;
+import java.time.Period;
 
+
+public class Persona {
+	
+	private int dni;
+	private String nombre;
+	private LocalDate fecha;
+	private String provincia;
+	
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona(int dni, String nombre, LocalDate fecha, String provincia) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fecha = fecha;
+		this.provincia = provincia;
+	}
+	
+	public Persona(int dni, String nombre, LocalDate fecha) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fecha = fecha;
+		this.provincia = "Jujuy";
+	}
+	
+	//public int calcular_edad(LocalDate fecha) {
+		//LocalDate fecha_actual= LocalDate.now();
+		//int anio_nac = fecha.getYear();
+		//int anio_actual = fecha_actual.getYear();
+		//int edad = anio_actual - anio_nac;
+		//return edad;
+	//}
+	
+	public int calcular_edad(LocalDate fecha) {
+		LocalDate fecha_actual= LocalDate.now();
+		int edad = Period.between(fecha, fecha_actual).getYears();
+		return edad;
+	}
+	
+	public Boolean mayor_menor(int edad) {
+		
+		if (edad<18)
+			return false;
+		else 
+			return true;
+	}
+	
+	public void getdatos(int edad) {
+		
+		if (mayor_menor(edad)==true) {
+			System.out.println("\nDNI: " + this.dni);
+			System.out.println("Nombre: " + this.nombre);
+			System.out.println("Fecha de nacimiento: " + this.fecha);
+			System.out.println("Edad: " + edad + "(es mayor de edad)");
+			System.out.println("Provincia: " + this.provincia);
+		}
+		else {
+			System.out.println("\nDNI: " + this.dni);
+			System.out.println("Nombre: " + this.nombre);
+			System.out.println("Fecha de nacimiento: " + this.fecha);
+			System.out.println("Edad: " + edad + "(es menor de edad)");
+			System.out.println("Provincia: " + this.provincia);
+		}
+		
+			
+		}
+	
+	
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(int anio, int mes,int dia) {
+		this.fecha = LocalDate.of(anio, mes, dia);
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	
+	
+	
+	
+ 
 }
