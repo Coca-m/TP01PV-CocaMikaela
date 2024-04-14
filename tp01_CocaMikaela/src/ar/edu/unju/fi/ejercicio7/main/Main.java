021596e697e34556152a22a5f4a8943a6e03ee66
seba_ <seba_@Compu>
2024-04-14 00:08:30 -0300

    finalizacion del ejercicio 7

--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio7/main/Main.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio7/main/Main.java
@@ -1,9 +1,26 @@
 package ar.edu.unju.fi.ejercicio7.main;
 
+import java.util.*;
+import ar.edu.unju.fi.ejercicio7.model.Empleado;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Ingrese el nombre del empleado: ");
+		String nombre = scan.nextLine();
+		System.out.println("Ingrese legajo: ");
+		int legajo = scan.nextInt();
+		System.out.println("Ingrese salario");
+		double salario = scan.nextDouble();
+		
+		Empleado empleado = new Empleado(nombre, legajo, salario);
+		empleado.mostrar(empleado);
+		empleado.aumento(empleado);
+		System.out.println("\nempleado con aumento");
+		empleado.mostrar(empleado);
 
 	}
 
--- tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
+++ tp01_CocaMikaela/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
@@ -1,5 +1,38 @@
 package ar.edu.unju.fi.ejercicio7.model;
 
-public class Empleado {
 
+public class Empleado {
+	
+	private String nombre;
+	private int legajo;
+	private double salario;
+	
+	final double SALARIO_MIN = 210000.00;
+	final double AUMENTO = 20000.00;
+	
+	
+	public Empleado(String nombre, int legajo, double salario) {
+		this.nombre = nombre;
+		this.legajo = legajo;
+		if (salario >= SALARIO_MIN) {
+		this.salario = salario;}
+		else {
+			System.out.println("el salario ingresado no cumple con el minimo establecido \nse procedera a establecer el minimo");
+			salario = SALARIO_MIN;
+			this.salario = salario;}
+	}
+	
+	public void mostrar(Empleado empleado) {
+		
+		System.out.println("Nombre del empleado: " + empleado.nombre);
+		System.out.println("Legajo: " + empleado.legajo);
+		System.out.println("Salario $: " + empleado.salario);
+	}
+	
+	public void aumento(Empleado empleado){
+		
+		empleado.salario = AUMENTO + empleado.salario;
+		
+	}
+	
 }
